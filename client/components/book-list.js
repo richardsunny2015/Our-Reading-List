02622a96881e68c689c995e768b6c68c6d2a693f
richardsunny2015 <richardsunny2015@gmail.com>
2019-03-07 11:59:11 -0500

    fix(book-list): Add authors and description to BookList component

--- client/components/book-list.js
+++ client/components/book-list.js
@@ -7,7 +7,9 @@ const BookList = ({books}) => {
         <div key={book.id}>
           <img src={book.volumeInfo.imageLinks.thumbnail} />
           <h4>Title: {book.volumeInfo.title}</h4>
+          <p>Author: {book.volumeInfo.authors.join(', ')}</p>
           <p>Page Count: {book.volumeInfo.pageCount}</p>
+          <p>Description: {book.volumeInfo.description}</p>
           <p>Published: {book.volumeInfo.publishedDate}</p>
         </div>
       ))}
