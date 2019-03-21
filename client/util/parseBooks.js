7763a15d49ac7b4a60f6317316abfeab199b6ed0
richardsunny2015 <richardsunny2015@gmail.com>
2019-03-21 16:11:09 -0400

    fix: Add title to fields

--- client/util/parseBooks.js
+++ client/util/parseBooks.js
@@ -1,14 +1,21 @@
 function parseBooks(books) {
-  const fields = ['authors', 'description', 'pageCount', 'publishedDate']
-  console.log('What is this?', books[0])
+  const fields = [
+    'authors',
+    'description',
+    'pageCount',
+    'publishedDate',
+    'title'
+  ]
   return books.map(book => {
     const parsed = {id: book.id}
     for (let i = 0; i < fields.length; i++) {
-      if (book.volumeInfo.hasOwnProperty(fields[i]))
+      if (book.volumeInfo.hasOwnProperty(fields[i])) {
         parsed[fields[i]] = book.volumeInfo[fields[i]]
+      }
     }
-    if (book.volumeInfo.hasOwnProperty('imageLinks'))
+    if (book.volumeInfo.hasOwnProperty('imageLinks')) {
       parsed.thumbnail = book.volumeInfo.imageLinks.thumbnail
+    }
     return parsed
   })
 }
